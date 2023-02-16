2d78ddfb069ce18d37e924f3292b44ac79c88c7a
Elias Daler <eliasdaler@protonmail.com>
2023-02-16 13:03:06 +0100

    Make sol2 and Lua work

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+#include <sol/sol.hpp>
+
 int main() {
     std::cout << "hello, world\n";
+
+    sol::state lua;
+    lua.open_libraries(sol::lib::base, sol::lib::package);
+    sol::protected_function_result result = lua.script("print('hello from Lua!')");
+    if (!result.valid()) {
+        sol::error err = result;
+        std::cout << err.what();
+    }
 }
