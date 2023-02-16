8d2c9627fdcd7970001385d9369d5aa0f8f3d007
Elias Daler <eliasdaler@protonmail.com>
2023-02-16 13:13:27 +0100

    Better stuff

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,10 @@
 #include <sol/sol.hpp>
 
 int main() {
-    std::cout << "hello, world\n";
-
+    std::cout << "Hello, from C++\n";
     sol::state lua;
     lua.open_libraries(sol::lib::base, sol::lib::package);
-    sol::protected_function_result result = lua.script("print('hello from Lua!')");
+    sol::protected_function_result result = lua.script("print('Hello from Lua')");
     if (!result.valid()) {
         sol::error err = result;
         std::cout << err.what();
